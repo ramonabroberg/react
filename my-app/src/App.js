ca7f067a71a38b32e78a3ae43defb6056b0ba6de
ramonabroberg <>
2023-12-01 15:54:54 +0000

    Add searchbar

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,10 +1,10 @@
 import './App.css';
-import LifeCyclesCWU from "./components/LifeCyclesCWU";
+import SearchBar from "./components/SearchBar";
 
 function App() {
   return (
     <div className="App">
-        <LifeCyclesCWU />
+        <SearchBar />
     </div>
   );
 }
